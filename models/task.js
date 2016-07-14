c9c23fdc3e312ff756b6f01e7e55058df8e83d42
supritkumar shah <suprit7@gmail.com>
2016-07-14 14:49:24 -0400

    model and .env file added

---
+++ models/task.js
@@ -0,0 +1,16 @@
+'use strict'
+const pg     = require('pg-promise')
+const env         = process.env.NODE_ENV || 'development';
+const DEV         = env==='development';
+const dotenv      = (DEV) ? require('dotenv').config() : undefined;
+
+const config = {
+  host:     process.env.DB_HOST,
+  port:     process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASS
+};
+
+const _db = pg(config)
+
